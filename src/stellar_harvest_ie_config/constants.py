4367752f7d9d7b7e713e4c7a7efc2a6f26815487
Machine Learning Maverick <github@machinelearningmaverick.com>
2026-04-01 18:53:11 +0100

    feat(constants): add constants in config module for other modules

---
+++ src/stellar_harvest_ie_config/constants.py
@@ -0,0 +1,15 @@
+# Kafka
+KAFKA_URI = "KAFKA_URI"
+
+# Kafka topic names
+KAFKA_TOPIC_SWPC = "KAFKA_TOPIC_SWPC"
+
+# Default timeouts & intervals
+HTTP_TIMEOUT_SECS = 10
+SCHEDULER_INTERVAL = 30  # minutes
+
+# API endpoints
+SWPC_API_URL = "https://services.swpc.noaa.gov/json/kvp-index.json"
+
+# Pydantic versions or schema IDs
+SCHEMA_VERSION = "2025-04-01"
